749ad5e1414ac9d5c654489cff0560d7f1b8624c
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-10-06 13:52:49 +0200

    edit dev url

--- va-loans/src/environments/environment.ts
+++ va-loans/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8080/api'
+  // serverUrl: 'http://localhost:8080/api'
+  serverUrl: "http://18.218.84.140:8080/api"
 };
 
 /*
