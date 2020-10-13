dfffbcd161292d3d54154de98ddc515eb652da07
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-10-13 07:31:10 +0200

    changed servers

--- config/env/development.js
+++ config/env/development.js
@@ -2,9 +2,9 @@ const token = process.env.api_token;
 
 module.exports = {
     app_name: 'VA-Loans',
-    api_url: "http://3.13.119.108:8080",
-    api_url_path: "http://3.13.119.108:8080/tidal/",
-    api_url_graphql: "http://3.13.119.108:8080/tidal/gql",
+    api_url: "http://54.215.66.118:8080",
+    api_url_path: "http://54.215.66.118:8080/tidal/",
+    api_url_graphql: "http://54.215.66.118:8080/tidal/gql",
     api_token: token
 };
 
--- va-loans/src/environments/environment.ts
+++ va-loans/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // serverUrl: 'http://localhost:8080/api'
-  serverUrl: "http://18.218.84.140:8080/api"
+  serverUrl: "http://13.52.125.105:8080/api"
 };
 
 /*
