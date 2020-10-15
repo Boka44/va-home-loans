c379fbee3347cd7900be3c3b3b24439558e689bb
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-10-15 15:05:59 +0200

    updated api url

--- config/env/development.js
+++ config/env/development.js
@@ -2,9 +2,9 @@ const token = process.env.api_token;
 
 module.exports = {
     app_name: 'VA-Loans',
-    api_url: "http://54.215.66.118:8080",
-    api_url_path: "http://54.215.66.118:8080/tidal/",
-    api_url_graphql: "http://54.215.66.118:8080/tidal/gql",
+    api_url: "https://admin.valoans.app",
+    api_url_path: "https://admin.valoans.app/tidal/",
+    api_url_graphql: "https://admin.valoans.app/tidal/gql",
     api_token: token
 };
 
