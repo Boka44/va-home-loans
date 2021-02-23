a36e435527c4cc9a449f8e1c02ec2fe327db5661
Nicholas A Hrboka <nicholashrboka@gmail.com>
2021-02-23 17:04:52 +0100

    added seo for blog posts

--- va-loans/src/app/blog-post/blog-post.component.ts
+++ va-loans/src/app/blog-post/blog-post.component.ts
@@ -1,3 +1,4 @@
+import { Meta, Title } from '@angular/platform-browser';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -13,7 +14,9 @@ export class BlogPostComponent implements OnInit {
 
   constructor(
     private _blogPostService: BlogPostService,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private title: Title,
+    private meta: Meta
   ) { }
 
   ngOnInit(): void {
@@ -25,12 +28,19 @@ export class BlogPostComponent implements OnInit {
     
   }
 
+  updateMeta(data) {
+    this.title.setTitle(data.seo_title + " | VaLoans" );
+    this.meta.updateTag({ name: 'description', content: data.seo_description})
+    this.meta.updateTag({ name: 'keywords', content: data.seo_tags})
+  }
+
   getBlogPost(id) {
     this._blogPostService.getBlogPost(id)
       .subscribe((result: any) => {
         let data = result.data.data.blog_posts.data[0];
         this.blogPost = data;
         this.isLoaded = true;
+        this.updateMeta(data);
       })
   }
 
